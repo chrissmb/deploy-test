ce65efedcd9ae58d123411edd8ba74e6d4d0f14f
Christopher Monteiro <chrissantista@gmail.com>
2023-02-15 20:44:49 -0300

    add assert

--- src/test/java/com/example/deploytest/DeployTestApplicationTests.java
+++ src/test/java/com/example/deploytest/DeployTestApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.deploytest;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class DeployTestApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertEquals(1, 1);
 	}
 
 }
